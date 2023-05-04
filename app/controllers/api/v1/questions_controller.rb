cd72ec24eed8418c8bf796e53c3c93557da1072c
Shubham <bhamshu@github.com>
2023-05-04 23:13:11 +0530

    +Feeling lucky

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -7,12 +7,18 @@ class Api::V1::QuestionsController < ApplicationController
             return
         end
 
-        @answer = QuestionService.new(strategy).answer(question)
-
+        if question == "I am feeling lucky"
+            ques_obj = Question.order("RANDOM()").limit(1).first
+            @question = ques_obj.question
+            @answer = ques_obj.answer
+        else
+            @question = question
+            @answer = QuestionService.new(strategy).answer(question)
+        end
         if @answer == nil
             render json: { error: 'Error occured. Unknown strategy?' }, status: 400
             return
         end
-        render json: { answer: @answer }
+        render json: {question: @question, answer: @answer }
     end
 end
